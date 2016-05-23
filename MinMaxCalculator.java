4cfb290eb4a9f324e83f94f3513a429df030861d
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-05-24 02:15:04 +0300

    Create MinMaxCalculator.java

---
+++ MinMaxCalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+/**
+ * Нахождение минимума/максимума двух чисел.
+ * int max(int a, int b)
+ * int min(int a, int b)
+ */
+public class MinMaxCalculator {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите число а = ");
+        int a = in.nextInt();
+        System.out.print("Введите число b = ");
+        int b = in.nextInt();
+        int max, min;
+        if (a > b){
+            max = a;
+            min = b;
+        }
+        else{
+            max = b;
+            min = a;
+        }
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+    }
+}
