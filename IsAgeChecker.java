f23f130e5a3c40c5e37b725654ab6797edaf9430
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2016-05-25 20:36:55 +0300

    Create IsAgeChecker.java

---
+++ IsAgeChecker.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+/**
+ * Определение корректности заданного возраста. Возраст может принимать значения
+ * в диапазоне 1..120.
+ */
+public class IsAgeChecker {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите возраст = ");
+        int age = in.nextInt();
+        boolean isAge = age > 0 & age < 121;
+        System.out.println(isAge);
+    }
+}
